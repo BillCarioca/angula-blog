4e5bb8e00e80f8ca231087913d232b233bbf39c8
Bill Carioca <billcarioca@hotmail.com>
2023-05-18 21:55:22 -0300

    Criação de paginas dinamicas de conteudos

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "photo":"https://1.bp.blogspot.com/-aODuKwNesxM/XuAPgd4g9uI/AAAAAAAC8MU/23lKtvSDDXcILGqeKCzz7PpSygxekH31wCLcBGAsYHQ/s1600/homemaranha2-aranhaversos.jpg",
+        "title":"HOMEM-ARANHA NO ARANHAVERSO 2: DIRETOR FALA SOBRE INCLUIR VERSÕES LIVE-ACTION DO HERÓI",
+        "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        "id":"2",
+        "photo":"https://th.bing.com/th/id/R.3416ae5986ea2ff23f8647212875e445?rik=lYq5Bn2mYqjv5Q&pid=ImgRaw&r=0",
+        "title":"Homem-Formiga e a Vespa: Quantumania já está disponivel na Disney+",
+        "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        "id":"3",
+        "photo":"https://s2.glbimg.com/KgaYZkbsUbDym2zv6h3fL3sZoqU=/0x0:1440x750/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/H/N/XSVeLpRhq1sdGZClBL8w/guardioes-da-galaxia-vol-3-justwatch-techtudo.webp",
+        "title":"Guardiões da Galáxia Vol. 3 estreia: 5 de maio",
+        "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        "id":"4",
+        "photo":"https://th.bing.com/th/id/R.547872d5549bf130712fbd05f255c5b6?rik=9BH0jYN%2bvL6s4A&pid=ImgRaw&r=0",
+        "title":"A segunda temporada da animação What If…? chegará ainda este ano",
+        "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    }
+]
